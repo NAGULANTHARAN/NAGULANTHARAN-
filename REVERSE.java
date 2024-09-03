c1ee20ac0a9be3edf1054f248fafac85c7ee6e4d
NAGULANTHARAN G <nagulantharan26@gmail.com>
2024-09-03 09:44:38 +0530

    Add files via upload

---
+++ REVERSE.java
@@ -0,0 +1,24 @@
+package IT;
+import java.io.*;
+import java.util.Scanner;
+
+public class REVERSE {
+    public static void main (String[] args) {
+      
+        String str= "Geeks", nstr="";
+        char ch;
+      
+      System.out.print("Original word: ");
+      System.out.println("Geeks");
+      
+      for (int i=0; i<str.length(); i++)
+      {
+        ch= str.charAt(i); 
+        nstr= ch+nstr; 
+      }
+      System.out.println("Reversed word: "+ nstr);
+    }
+}
+
+
+
